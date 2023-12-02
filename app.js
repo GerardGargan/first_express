e15abaa3e62336f8953ae5db40eb1fc72da3ba7a
Gerard Gargan <gerardgargan@hotmail.co.uk>
2023-12-02 13:41:46 +0000

    added router object for routing

--- app.js
+++ app.js
@@ -1,31 +1,34 @@
 const express = require('express');
+const router = express.Router();
 
 var app = express();
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + "/public"));
 
-app.get("/", (req,res) => {
+router.get("/", (req,res) => {
     const details = {name: 'Gerard', favorite: 'Chinese'};
     res.status(200);
     res.render('generic',details);
 });
 
-app.get("/first", (req,res) => {
+router.get("/first", (req,res) => {
     const details = {name: 'Caoimhe', favorite: 'Pasta'};
     res.status(200);
     res.render('generic', details);
 });
 
-app.get("/second", (req,res) => {
+router.get("/second", (req,res) => {
     const details = {name: 'james', favorite: 'mexican'};
     res.status(200);
     res.render('generic', details);
 });
 
-app.get("*", (req,res) => {
+router.get("*", (req,res) => {
     res.status(404);
     res.send("<h1>Page not found</h1>");
 });
+
+app.use('/', router);
 
 app.listen(3000, () => {
     console.log("Listening on port 3000");
