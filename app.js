d4c638f6b074c3592190ba9b27199c95e47dad3f
Gerard Gargan <gerardgargan@hotmail.co.uk>
2023-12-02 13:05:10 +0000

    added html files for first and second pages

--- app.js
+++ app.js
@@ -3,18 +3,22 @@ const express = require('express');
 var app = express();
 
 app.get("/", (req,res) => {
+    res.status(200);
     res.send("<h1>Hello Express World!</h1>")
 });
 
 app.get("/first", (req,res) => {
+    res.status(200);
     res.send("<h1>First page</h1>");
 });
 
 app.get("/second", (req,res) => {
+    res.status(200);
     res.send("<h1>Second page</h1>");
 });
 
 app.get("*", (req,res) => {
+    res.status(404);
     res.send("<h1>Page not found</h1>");
 });
 
