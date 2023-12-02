bb17c661874566bb2788b6a0a8db7a11645b9d1b
Gerard Gargan <gerardgargan@hotmail.co.uk>
2023-12-02 13:00:01 +0000

    set up node server listening on port 3000, added some elementary routing

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
-const app = require('express');
+const express = require('express');
+
+var app = express();
+
+app.get("/", (req,res) => {
+    res.send("<h1>Hello Express World!</h1>")
+});
+
+app.get("/first", (req,res) => {
+    res.send("<h1>First page</h1>");
+});
+
+app.get("/second", (req,res) => {
+    res.send("<h1>Second page</h1>");
+});
+
+app.get("*", (req,res) => {
+    res.send("<h1>Page not found</h1>");
+});
+
+app.listen(3000, () => {
+    console.log("Listening on port 3000");
+});
